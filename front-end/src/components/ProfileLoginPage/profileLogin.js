7cb2700eccaf237dda610c4c6237f3f892db1c3a
ankitgoyal0106 <ankitgoyal@umass.edu>
2024-11-06 14:07:06 -0500

    edit

--- front-end/src/components/ProfileLoginPage/profileLogin.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.password-toggle').addEventListener('click', function () {
-    const passwordField = document.getElementById('password');
-    if (passwordField.type === 'password') {
-        passwordField.type = 'text';
-    } else {
-        passwordField.type = 'password';
-    }
-});
