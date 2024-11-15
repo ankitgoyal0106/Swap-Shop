d314d965d57ff6f6bccdfa990613521be4f92567
Nathan P. <ncp294@gmail.com>
2024-11-14 22:45:03 -0500

    CSS Fixes

--- front-end/src/components/homePage/homePage.js
+++ front-end/src/components/homePage/homePage.js
@@ -1,8 +1,16 @@
 import { BaseComponent } from "../BaseComponent/BaseComponent.js";
 
 export class homeComponent extends BaseComponent{
+    #container = null;
+
+    constructor() {
+      super();
+      this.loadCSS('homePage');
+    }
+
     render() {
       const homePage = document.createElement('div');
+      homePage.id = "home-page";
       homePage.innerHTML = `
         <h2>Home Page</h2>
         <p>Welcome to the home page! Here is some interesting content.</p>
